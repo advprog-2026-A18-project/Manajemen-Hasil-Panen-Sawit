185935a114fb2eebc09d71c6bf3fbbc748329034
FrankyRay <frankyraymarcellsinaga@gmail.com>
2026-02-20 12:20:08 +0700

    ini: initial init

---
+++ src/test/java/id/ac/ui/cs/advprog/sawitpanen/SawitPanenApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.sawitpanen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SawitPanenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
