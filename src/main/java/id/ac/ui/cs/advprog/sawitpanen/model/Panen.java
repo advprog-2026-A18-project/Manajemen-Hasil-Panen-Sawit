448d58074dd93c35e8fcb3748ea3b12da556b55a
FrankyRay <frankyraymarcellsinaga@gmail.com>
2026-02-20 20:20:03 +0700

    chg: rewrite model and repository for database integration

--- src/main/java/id/ac/ui/cs/advprog/sawitpanen/model/Panen.java
+++ src/main/java/id/ac/ui/cs/advprog/sawitpanen/model/Panen.java
@@ -1,21 +1,44 @@
 package id.ac.ui.cs.advprog.sawitpanen.model;
 
+import jakarta.persistence.*;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 
+@Entity
+@Table(name = "panen")
 @Getter @Setter
 public class Panen {
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
-    private int buruhId;
+    @Column(name = "buruh_id", nullable = false)
+    private UUID buruhId;
+
+    @Column(nullable = false)
     private int kuantitas;
+
+    @JdbcTypeCode(SqlTypes.ARRAY)
+    @Column(name = "bukti_foto")
     private List<String> buktiFoto;
 
-    private int tanggalMulai;
-    private int tanggalAkhir;
-    private int tanggalPanen;
+    @Column(name = "tanggal_mulai")
+    private LocalDate tanggalMulai;
 
+    @Column(name = "tanggal_akhir")
+    private LocalDate tanggalAkhir;
+
+    @Column(name = "tanggal_panen")
+    private LocalDate tanggalPanen;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private StatusPanen status;
 }
--- src/main/java/id/ac/ui/cs/advprog/sawitpanen/service/PanenService.java
+++ src/main/java/id/ac/ui/cs/advprog/sawitpanen/service/PanenService.java
@@ -3,8 +3,10 @@ package id.ac.ui.cs.advprog.sawitpanen.service;
 import id.ac.ui.cs.advprog.sawitpanen.model.Panen;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface PanenService {
-    public Panen create(Panen panen);
+    public Panen lapor(Panen panen);
     public List<Panen> findAll();
+    public List<Panen> findByBuruhId(UUID buruhId);
 }
