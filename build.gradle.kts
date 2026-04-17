a2e55e28a17d01e4b493673f1557777086e457c2
FrankyRay <frankyraymarcellsinaga@gmail.com>
2026-04-17 21:19:00 +0700

    test: change all unit-test for api changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,26 +41,26 @@ val junitJupiterVersion = "5.9.1"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.postgresql:postgresql")
+
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     annotationProcessor("org.projectlombok:lombok")
-    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
     testImplementation("org.seleniumhq.selenium:selenium-java:$seleniumJavaVersion")
     testImplementation("io.github.bonigarcia:webdrivermanager:${webdriverManagerVersion}")
     testImplementation("io.github.bonigarcia:selenium-jupiter:${seleniumJupiterVersion}")
-//    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
-//    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
-
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
-    testRuntimeOnly("com.h2database:h2")
 
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.postgresql:postgresql")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testRuntimeOnly("com.h2database:h2")
 }
 
 tasks.test {
--- src/main/java/id/ac/ui/cs/advprog/sawitpanen/dto/CreatePanenRequest.java
+++ src/main/java/id/ac/ui/cs/advprog/sawitpanen/dto/CreatePanenRequest.java
@@ -1,5 +1,8 @@
 package id.ac.ui.cs.advprog.sawitpanen.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.antlr.v4.runtime.misc.NotNull;
@@ -10,8 +13,18 @@ import java.util.UUID;
 @Data
 @AllArgsConstructor
 public class CreatePanenRequest {
+
+    @NotNull()
     private UUID buruhId;
+
+    @Min(value = 1, message = "Kuantitas panen minimal 1 kg")
     private int kuantitasBerat;
+
+    @NotBlank(message = "Berita panen tidak boleh kosong")
     private String berita;
+
+    @NotNull()
+    @Size(min = 1, message = "Minimal harus ada 1 bukti foto")
     private List<String> buktiFoto;
+
 }
