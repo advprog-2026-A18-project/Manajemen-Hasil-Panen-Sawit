20a49865ac8e8b263c90fa94fbbacca9803022ab
Evan Haryo Widodo <evan.haryo@ui.ac.id>
2026-05-08 20:21:06 +0700

    config: add postgresql runtime

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,8 +59,9 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
     testImplementation("com.h2database:h2")
-
+    
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    runtimeOnly("org.postgresql:postgresql")
 }
 
 tasks.test {
